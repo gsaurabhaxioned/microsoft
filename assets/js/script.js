75a6efce6168938c45dde3fa6c8cf4f84f3092bc
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-17 21:00:54 +0530

    js code added for hamburger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
-/* Author: 
-
-*/
-
-
+$(document).ready(function () {
+    $(".ham").click(function () {
+        $(".menu").slideToggle();
+        $(this).toggleClass("cross");
+    });
+});
 
 
 
