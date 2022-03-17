2b1f7fbd45ca173cae25f8b8357a0ba29c1e142d
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-17 21:20:14 +0530

    js added for video functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,37 @@ $(document).ready(function () {
     });
 });
 
+let play_button = document.querySelector('.play-button'),
+play_video = document.querySelector('.play-video'),
+close_video = document.querySelector('.close-video'),
+vid = document.querySelector('.modal-video'),
+modal_back = document.querySelector('.modal-background');
+
+
+play_button.addEventListener('click',()=>{
+    play_video.style.display = "block";
+    close_video.style.display = "block";
+    modal_back.style.display = "block";
+    vid.play();
+})
+
+close_video.addEventListener('click',()=>{
+    play_video.style.display = "none";
+    close_video.style.display = "none";
+    modal_back.style.display = "none";
+    vid.pause(); 
+})
+
+modal_back.addEventListener('click',()=>{
+    play_video.style.display = "none";
+    close_video.style.display = "none";
+    modal_back.style.display = "none";
+    vid.pause(); 
+})
+
+
+
+
 
 
 
