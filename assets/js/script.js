b071902e3f1e72abcec1166b140fab51eb6828ea
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-18 14:48:50 +0530

    js code formatted

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,53 +6,29 @@ $(document).ready(function () {
 });
 
 let play_button = document.querySelector('.watch-video'),
-play_video = document.querySelector('.play-video'),
-close_video = document.querySelector('.close-video'),
-vid = document.querySelector('.modal-video'),
-modal_back = document.querySelector('.modal-background');
+    play_video = document.querySelector('.play-video'),
+    close_video = document.querySelector('.close-video'),
+    vid = document.querySelector('.modal-video'),
+    modal_back = document.querySelector('.modal-background');
 
 
-play_button.addEventListener('click',()=>{
+play_button.addEventListener('click', () => {
     play_video.style.display = "block";
     close_video.style.display = "block";
     modal_back.style.display = "block";
     vid.play();
 })
 
-close_video.addEventListener('click',()=>{
+close_video.addEventListener('click', () => {
     play_video.style.display = "none";
     close_video.style.display = "none";
     modal_back.style.display = "none";
-    vid.pause(); 
+    vid.pause();
 })
 
-modal_back.addEventListener('click',()=>{
+modal_back.addEventListener('click', () => {
     play_video.style.display = "none";
     close_video.style.display = "none";
     modal_back.style.display = "none";
-    vid.pause(); 
+    vid.pause();
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
